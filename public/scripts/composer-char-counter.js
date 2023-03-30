1b123f875abedc679e75479da3d165dc5a48c765
Mike <mike.simnes@gmail.com>
2023-03-29 17:30:59 -0700

    add textarea resizing, add toggle function to file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+
+  // toggle new tweet button in nav bar
+  $("#toggle-new-tweet").on('click', function() {
+    $(".new-tweet").slideDown("slow", function() {
+      $(this).css("display", "flex");
+      $("#tweet-text").focus();
+    });
+  });
+  // handle textarea sizing and character counter
   $("#tweet-text").on("input", function() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
     let tweetLength = $(this).val().length;
     let charactersLeft = 140 - tweetLength;
     $(".counter").text(charactersLeft);
